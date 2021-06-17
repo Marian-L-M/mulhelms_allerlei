5078b1f12b70e58cd5fc57c33e79e7496c16ae4a
Marian-L-M <marianlmaschke@gmail.com>
2021-06-17 16:00:55 +0900

    adjusted html/css/img structure to suit react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a href="" className="logo">Mulhems Allerlei</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <main>
+        <div className="row center">
+            <div className="card">
+                <a href="product.html">
+                    <img className="medium" src="./img/product_sample.jpg" alt="placeholder product"/>
+                </a>
+                <div className="card-body">
+                    <a href="product.html">
+                        <h2>Chicklet Normalsize White</h2>
+                    </a>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star-half" aria-hidden="true"></i></span>
+                    </div>
+                    <div className="price">
+                        $120
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <a href="product.html">
+                    <img className="medium" src="./img/product_sample.jpg" alt="placeholder product"/>
+                </a>
+                <div className="card-body">
+                    <a href="product.html">
+                        <h2>Chicklet Normalsize White</h2>
+                    </a>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                    </div>
+                    <div className="price">
+                        $120
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <a href="product.html">
+                    <img className="medium" src="./img/product_sample.jpg" alt="placeholder product"/>
+                </a>
+                <div className="card-body">
+                    <a href="product.html">
+                        <h2>Chicklet Normalsize White</h2>
+                    </a>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                    </div>
+                    <div className="price">
+                        $120
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <a href="product.html">
+                    <img className="medium" src="./img/product_sample.jpg" alt="placeholder product"/>
+                </a>
+                <div className="card-body">
+                    <a href="product.html">
+                        <h2>Chicklet Normalsize White</h2>
+                    </a>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                    </div>
+                    <div className="price">
+                        $120
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <a href="product.html">
+                    <img className="medium" src="./img/product_sample.jpg" alt="placeholder product"/>
+                </a>
+                <div className="card-body">
+                    <a href="product.html">
+                        <h2>Chicklet Normalsize White</h2>
+                    </a>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                    </div>
+                    <div className="price">
+                        $120
+                    </div>
+                </div>
+            </div>
+        </div>
+      </main>
+      <footer className="row center">All rights reserved</footer>
     </div>
   );
 }
